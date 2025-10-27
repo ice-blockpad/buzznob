d296e41b3403fb0c114989f758a69d07c4736b40
ice-blockpad <just2williamz@gmail.com>
2025-10-27 15:20:11 +0100

    wallet

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -7,22 +7,18 @@ const SALT = 'buzznob-wallet-salt';
  * Encrypt private key with user's password
  */
 function encryptPrivateKey(privateKey, password) {
-  const algorithm = 'aes-256-gcm';
+  const algorithm = 'aes-256-cbc';
   const key = crypto.scryptSync(password, SALT, 32);
   const iv = crypto.randomBytes(16);
   
-  const cipher = crypto.createCipherGCM(algorithm, key, iv);
-  cipher.setAAD(Buffer.from('buzznob-wallet', 'utf8'));
+  const cipher = crypto.createCipher(algorithm, key);
   
   let encrypted = cipher.update(privateKey, 'utf8', 'hex');
   encrypted += cipher.final('hex');
   
-  const authTag = cipher.getAuthTag();
-  
   return {
     encrypted,
     iv: iv.toString('hex'),
-    authTag: authTag.toString('hex'),
     algorithm
   };
 }
@@ -31,14 +27,11 @@ function encryptPrivateKey(privateKey, password) {
  * Decrypt private key with user's password
  */
 function decryptPrivateKey(encryptedData, password) {
-  const algorithm = 'aes-256-gcm';
+  const algorithm = 'aes-256-cbc';
   const key = crypto.scryptSync(password, SALT, 32);
   const iv = Buffer.from(encryptedData.iv, 'hex');
-  const authTag = Buffer.from(encryptedData.authTag, 'hex');
   
-  const decipher = crypto.createDecipherGCM(algorithm, key, iv);
-  decipher.setAAD(Buffer.from('buzznob-wallet', 'utf8'));
-  decipher.setAuthTag(authTag);
+  const decipher = crypto.createDecipher(algorithm, key);
   
   let decrypted = decipher.update(encryptedData.encrypted, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
