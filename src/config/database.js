412f4a47d562b6c635f023a243932793075230b8
ice-blockpad <just2williamz@gmail.com>
2025-11-11 23:10:17 +0100

    backend

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,7 @@ const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
 });
 
+
 const connectDB = async () => {
   try {
     await prisma.$connect();
